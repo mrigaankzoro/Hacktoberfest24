d9b6c0321592c78accb44574677807a642061981
omjain1 <jkapish000@gmail.com>
2024-10-08 23:56:24 +0530

    Largest Number

--- Largest_number.cpp
+++ Largest_number.cpp
@@ -1,6 +1,6 @@
 // C++ program to find the largest number in an array
 // without using std::max_element
-#include <algorithm>
+// Simplified Version 
 #include <iostream>
 
 using namespace std;
@@ -11,12 +11,18 @@ int main()
     int arr[] = { 1, 45, 54, 71, 76, 12 };
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    // Find the largest element in the array
-    int max_num = *max_element(arr, arr + n);
+    // Initialize the max_num with the first element of the array
+    int max_num = arr[0];
+
+    // Traverse the array to find the largest number
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max_num) {
+            max_num = arr[i];
+        }
+    }
 
     // Output the result
-    cout << "Largest number in the array is: " << max_num
-         << endl;
+    cout << "Largest number in the array is: " << max_num << endl;
 
     return 0;
 }
