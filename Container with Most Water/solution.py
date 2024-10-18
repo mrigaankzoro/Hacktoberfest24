50b85f4ac6c2169645fcfbf7d218e1c07f94a44b
vishalsarvanis <parker75986@gmail.com>
2024-10-18 15:51:56 +0530

    Update solution.py

--- Container with Most Water/solution.py
+++ Container with Most Water/solution.py
@@ -1,13 +1,18 @@
 from typing import List
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         max_area = 0
         left = 0
-        right = len(height) -1
+        right = len(height) - 1
 
         while left < right:
-            max_area = max(max_area, (right - left), min(height[left], height[right]))
-                           
+            # Calculate the width and area
+            width = right - left
+            current_area = width * min(height[left], height[right])
+            max_area = max(max_area, current_area)
+
+            # Move the pointers
             if height[left] < height[right]:
                 left += 1
             else:
