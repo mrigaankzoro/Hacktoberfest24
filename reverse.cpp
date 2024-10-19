123e1b107b0c9f7d2cccf70931d048d7f7aca155
cayrinr <103485549+cayrinr@users.noreply.github.com>
2024-10-19 08:55:19 +0530

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -1,19 +1,68 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
-void reverse(int i, int arr[], int n){
-    if(i>=n/2){
-        return;
-    }
-    swap(arr[i],arr[n-i-1]);
-    reverse(i+1,arr,n);
-}
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
 
-int main(){
-    int arr[5]={1,2,3,4,5};
-    reverse(0,arr,5);
-    for (int i = 0; i < 5 ; i++)
-    {
-        cout<<arr[i];
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() : head(nullptr) {}
+
+    void append(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
     }
+
+    void reverse() {
+        Node* prev = nullptr;
+        Node* current = head;
+        Node* next = nullptr;
+
+        while (current) {
+            next = current->next; // Store next node
+            current->next = prev; // Reverse the link
+            prev = current;       // Move prev forward
+            current = next;       // Move current forward
+        }
+        head = prev; // Update head to new first element
+    }
+
+    void print() {
+        Node* temp = head;
+        while (temp) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+
+    std::cout << "Original List: ";
+    list.print();
+
+    list.reverse();
+
+    std::cout << "Reversed List: ";
+    list.print();
+
+    return 0;
 }
