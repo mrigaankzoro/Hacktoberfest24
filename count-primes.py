a4aa181fe678e26c7e4b77f05552711586b393a2
Niharika <niharikah005@gmail.com>
2024-10-20 22:58:41 +0530

    Refined comments

--- count-primes.py
+++ count-primes.py
@@ -3,17 +3,16 @@ class Solution:
         # no primes lesser than 2
         if n<2:
             return 0
-        # initialise a list with n True's
+        # initialise a list with n True's, assume all numbers >=2 are prime
         primes = [True]*n
         ans = 0
         for i in range(2,n):
-            # if prime then inc count
+            # if prime then increment count
             if primes[i]:
                 ans+=1
-                # mark all multiples of i as false
-                # how the for loop works:
-                # start from i*i cuz till that number, all the prev nums would have marked the necessary numbers
-                # go till n with steps of i (basically multiples of i)
+                '''mark all multiples of i (starting from i*i) as non-prime 
+                start at i*i because smaller multiples of i (like i*2, i*3, ...) 
+                will have already been marked by previous primes'''
                 for j in range(i*i,n,i):
                     primes[j] = False
 
